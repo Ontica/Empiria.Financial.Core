3f881ee42df0a27493daef8510cb56064c0761be
José Manuel Cota <jmcota@ontica.org>
2024-05-18 09:44:51 -0600

    Empiria Financial Core. Initial commit.

---
+++ Core.Tests/Empiria.Financial.Core.Tests.AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿/*Empiria Financial ******************************************************************************************
+*                                                                                                            *
+*  System   : Financial Core                               Module  : Core Types and Services Tests           *
+*  Assembly : Empiria.Financial.Core.dll                   Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Tests suite for Empiria Financial Core use cases, types and services.                          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
---
+++ Core/Empiria.Financial.Core.AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿/*Empiria Financial ******************************************************************************************
+*                                                                                                            *
+*  System   : Financial Core                               Module  : Core Types and Services                 *
+*  Assembly : Empiria.Financial.Core.dll                   Pattern : Assembly Attributes File                *
+*                                                          License : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Contains general purpose uses cases, types and services used for financial solutions.          *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+/*************************************************************************************************************
+* Assembly configuration attributes.                                                                         *
+*************************************************************************************************************/
+[assembly: AssemblyTrademark("Empiria and Ontica are either trademarks of La Vía Óntica SC or Ontica LLC.")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("Empiria.Financial.Core.Tests")]
