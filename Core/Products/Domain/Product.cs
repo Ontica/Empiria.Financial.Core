bf7eba7c293e55d1b0c634aff7aa83e929e0878f
José Manuel Cota <jmcota@ontica.org>
2024-09-09 15:21:58 -0400

    Product and ProductUnit types were added

---
+++ Core/Products/Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Products Management                        Component : Domain Layer                            *
+*  Assembly : Empiria.Financial.Core.dll                 Pattern   : Information Holder                      *
+*  Type     : Product                                    License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a good or a service.                                                                *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Products {
+
+  /// <summary>Represents a good or a service.</summary>
+  public class Product : BaseObject {
+
+  } // class Product
+
+}  // namespace Empiria.Products
---
+++ Core/Products/Domain/ProductUnit.cs
@@ -0,0 +1,18 @@
+﻿/* Empiria Financial *****************************************************************************************
+*                                                                                                            *
+*  Module   : Products Management                        Component : Domain Layer                            *
+*  Assembly : Empiria.Financial.Core.dll                 Pattern   : Information Holder                      *
+*  Type     : ProductUnit                                License   : Please read LICENSE.txt file            *
+*                                                                                                            *
+*  Summary  : Represents a unit of measure of a product.                                                     *
+*                                                                                                            *
+************************* Copyright(c) La Vía Óntica SC, Ontica LLC and contributors. All rights reserved. **/
+
+namespace Empiria.Products {
+
+  /// <summary>Represents a unit of measure of a product.</summary>
+  public class ProductUnit : GeneralObject {
+
+  } // class ProductUnit
+
+} // namespace Empiria.Products
